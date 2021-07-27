9c2849dc0c06c76542ce6cb300b1aed5b9130538
Carlos Martinez <curlosmart@gmail.com>
2021-07-26 21:30:09 -0400

    Added styling to html with css and updated alert

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-const numOfDiceElem = document.getElementById('num-of-dice')
+const numOfDiceElem = document.getElementById('numOfDice')
 const sidesElem = document.getElementById('sides')
-const rollButtonElem = document.getElementById('roll-button')
+const rollButtonElem = document.getElementById('rollButton')
 const dice = document.getElementById('dice')
 
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * ((max + 1) - min)) + min
 }
 
-const displayErrorMessage = (message) => {
-    // Display error message, change div class list and css to not be hidden
-}
-
 const rollDice = () => {
     let numOfDice = Number(numOfDiceElem.value)
     let sides = Number(sidesElem.value)
     console.log(numOfDice, sides)
-    if (numOfDice == 0 || isNaN(numOfDice)) {
-        displayErrorMessage("Please enter a number")
+    if (numOfDice == 0) {
+        alert("Please enter a dice amount.")
         return
-    } 
+    }
     
     if (numOfDice >= 1 && numOfDice <= 50) {
         let results = []
@@ -29,7 +25,7 @@ const rollDice = () => {
 
         displayDice(results)
     } else {
-        displayErrorMessage("Please enter a number from 1 to 50")
+        alert("Please enter a dice amount from 1 to 50")
     }
 }
 
@@ -48,7 +44,7 @@ const createDie = (faceNumber) => {
 }
 
 const displayDice = (results) => {
-
+    dice.innerHTML = ''
     console.log(results)
 
     for (let faceNumber of results) {
